2267f4f1a4688bcebcd31ee1636a71b1f7e781b5
Guillermo <vital_tmv@yahoo.com>
2024-05-20 14:13:52 -0500

    complete

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,13 +1,32 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+
 
 function Directors() {
+  const [directors, setDirectors] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/directors")
+    .then(res => res.json())
+    .then(data => setDirectors(data))
+    .catch(error => console.error(error))
+  },[])
+
+  const director = directors.map(director => {
+    return <article key={director.id}>
+      <h2>{director.name}</h2>
+      <ul>{director.movies.map(movie => <li key={movie.id}>{movie}</li>)}</ul>
+    </article>
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {director}
       </main>
     </>
   );
